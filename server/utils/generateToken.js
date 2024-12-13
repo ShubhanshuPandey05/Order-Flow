fecf9c730b050e91ac91207954bd5226bc641e8b
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2024-12-13 17:20:06 +0530

    same issue

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateAndSetCookies = (userId) => {
     console.log(userId);
-    return jwt.sign({userId},process.env.SECRET)
+    return jwt.sign({userId},process.env.SECRET,{expiresIn:'100y'})
 }
 
 export default generateAndSetCookies;
