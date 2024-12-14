c63e8948e943161a13e8b96b6bf17095ee0cf9b7
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2024-12-14 11:33:10 +0530

    cors

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const genrateTokenAndSetCookies = (userId,res) => {
-    const token = jwt.sign({userId},process.env.JWT_SECRET, {
+    const token = jwt.sign({userId},process.env.SECRET, {
         expiresIn: "999d"
     });
 
